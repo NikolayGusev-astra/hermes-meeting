8bcb99fa651911302089cc29f9edc9c0d573e6a0
NikolayGusev-astra <198532749+NikolayGusev-astra@users.noreply.github.com>
2026-08-13 20:53:56 +0500

    fix: import useEffect (краш ProgressPanel 'useEffect is not defined')

--- com.hermes.desktop/plugin.js
+++ com.hermes.desktop/plugin.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { host, useQuery, useQueryClient, ROUTES_AREA, SIDEBAR_NAV_AREA, PALETTE_AREA, THEMES_AREA } from '@hermes/plugin-sdk'
 import { jsx, jsxs } from 'react/jsx-runtime'
 
